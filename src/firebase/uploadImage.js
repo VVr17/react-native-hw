e075edab371080b9fb6db058b272db71fe16ea4d
Vira Voronova <vera.voronova@hotmail.com>
2023-01-27 16:17:14 +0100

    upload image fixed

--- src/firebase/uploadImage.js
+++ src/firebase/uploadImage.js
@@ -1,12 +1,11 @@
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "./config";
-import { nanoid } from "nanoid";
 
 export const uploadImage = async (image) => {
   try {
     const response = await fetch(image);
     const file = await response.blob(); // change format to Blob
-    const postId = nanoid(); // add Id
+    const postId = Date.now();
 
     const imageRef = ref(storage, `postImages/${postId}`);
 
