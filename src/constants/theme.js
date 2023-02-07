bcb26498e7c2a191caacc2f5f08f7501b87a8df0
Vira Voronova <vera.voronova@hotmail.com>
2023-02-07 11:27:37 +0100

    firebase config changed

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -2,8 +2,8 @@ export const theme = {
   colors: {
     mainText: "#212121",
     secondaryText: "rgba(33, 33, 33, 0.8)",
-    accent: "#FF6C00",
-    active: "#ed6807",
+    accent: "#F4A92A",
+    active: "#B9651F",
     secondaryAccent: "#1B4371",
     mainBackground: "#ffffff",
     inputBackground: "#F6F6F6",
